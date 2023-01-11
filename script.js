83be634000bcd3651843005b43c63846e190d06e
Fortwin <basirathamz@gmail.com>
2023-01-11 17:01:30 +0100

    Fixed read more button

---
+++ script.js
@@ -0,0 +1,84 @@
+function myFunction(){
+    var dots = document.getElementById('dots');
+    var readMore = document.getElementById('more');
+    var readText = document.getElementById('myTag');
+
+    if(dots.style.display === 'none') {
+        dots.style.display = 'inline';
+        readText.innerHTML = 'Read More';
+        readMore.style.display = 'none';
+    } else{
+        dots.style.display = 'none';
+        readText.innerHTML = 'see less';
+        readMore.style.display = 'inline'        
+         
+    }
+}
+
+function theFunction(){
+    var dot = document.getElementById('dot');
+    var readMore = document.getElementById('big');
+    var readText = document.getElementById('theTag');
+
+    if(dot.style.display === 'none') {
+        dot.style.display = 'inline';
+        readText.innerHTML = 'Read More';
+        readMore.style.display = 'none';
+    } else{
+        dot.style.display = 'none';
+        readText.innerHTML = 'see less';
+        readMore.style.display = 'inline'        
+         
+    }
+}
+
+function yourFunction(){
+    var dotss = document.getElementById('dotss');
+    var readMore = document.getElementById('large');
+    var readText = document.getElementById('yourTag');
+
+    if(dotss.style.display === 'none') {
+        dotss.style.display = 'inline';
+        readText.innerHTML = 'Read More';
+        readMore.style.display = 'none';
+    } else{
+        dotss.style.display = 'none';
+        readText.innerHTML = 'see less';
+        readMore.style.display = 'inline'        
+         
+    }
+}
+
+function ourFunction(){
+    var dotts = document.getElementById('dotts');
+    var readMore = document.getElementById('fat');
+    var readText = document.getElementById('ourTag');
+
+    if(dotts.style.display === 'none') {
+        dotts.style.display = 'inline';
+        readText.innerHTML = 'Read More';
+        readMore.style.display = 'none';
+    } else{
+        dotts.style.display = 'none';
+        readText.innerHTML = 'see less';
+        readMore.style.display = 'inline'        
+         
+    }
+}
+
+function herFunction(){
+    var dottss = document.getElementById('dottss');
+    var readMore = document.getElementById('small');
+    var readText = document.getElementById('herTag');
+
+    if(dottss.style.display === 'none') {
+        dottss.style.display = 'inline';
+        readText.innerHTML = 'Read More';
+        readMore.style.display = 'none';
+    } else{
+        dottss.style.display = 'none';
+        readText.innerHTML = 'see less';
+        readMore.style.display = 'inline'        
+         
+    }
+}
